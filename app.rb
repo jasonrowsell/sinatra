54d35510da033a35342667f2ab3ed1c4a6123c4f
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-12 21:31:30 +0100

    Added views and runfile

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+  get '/' do
+    "Hello World"
+  end
+
+  get '/secret' do
+    "🤠"
+  end
+
+  get '/reload' do
+    "🔫"
+  end
+
+  get '/random-cat' do
+    @name = ["Amigo", "Misty", "Almond"].sample
+    erb(:index)
+  end
+
+  get '/cat-form' do
+    erb(:cat_form)
+  end
+
+  post '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
+  end
+
